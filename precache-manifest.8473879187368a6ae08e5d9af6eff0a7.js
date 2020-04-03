be639dce7a349cb838e66fb79c57eadc1015951d
andi-irsandi <andi.ramadani765@gmail.com>
2020-04-03 08:00:55 +0700

    deploy

---
+++ precache-manifest.8473879187368a6ae08e5d9af6eff0a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1809e6a074d6210071d68816f08dcaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f70bb7b9b0be0c20f51",
+    "url": "/static/css/main.fa53b4c2.chunk.css"
+  },
+  {
+    "revision": "4bc6f6f158fede06bfaf",
+    "url": "/static/js/2.2f9b590a.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.2f9b590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f70bb7b9b0be0c20f51",
+    "url": "/static/js/main.258c8830.chunk.js"
+  },
+  {
+    "revision": "e6953adddfc4d445eac3",
+    "url": "/static/js/runtime-main.57d42395.js"
+  },
+  {
+    "revision": "877635d8af9f840aaec26d7e54c2e7b0",
+    "url": "/static/media/coffee.877635d8.png"
+  },
+  {
+    "revision": "897aa1ea50893f67cd24a8d6f3316249",
+    "url": "/static/media/secure.897aa1ea.jpg"
+  }
+]);
